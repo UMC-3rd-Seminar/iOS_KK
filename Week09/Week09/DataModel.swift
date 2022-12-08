775a56a9e4c43b1f4509ba879d8a83c3f5792185
lorlorv <dev.eunseo@gmail.com>
2022-12-08 20:09:03 +0900

    [#8] Feat : Week08 Standard Mission

---
+++ Week09/Week09/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  Week09
+//
+//  Created by eunseo on 2022/11/24.
+//
+
+import Foundation
---
+++ Week09/Week09/RequestFile.swift
@@ -0,0 +1,8 @@
+//
+//  RequestFile.swift
+//  Week09
+//
+//  Created by eunseo on 2022/11/25.
+//
+
+import Foundation
